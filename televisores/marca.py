de0e3908fc2e21381f0acf7a4e17bc2f04f2dbe5
kevinramos2 <keramosl@unal.edu.co>
2023-08-25 18:21:52 -0500

    creacion clases

---
+++ televisores/marca.py
@@ -0,0 +1,7 @@
+class Marca:
+    def __init__(self, nombre):
+        self.nombre = nombre
+    def setNombre(self, nombre):
+        self.nombre = nombre
+    def getNombre(self, nombre):
+        return self.nombre
