aad509fe5f74a85236ce31a176f2f2f640810a05
kevinramos2 <keramosl@unal.edu.co>
2023-08-27 12:21:57 -0500

    Update tv.py
    get y set de atributos, conteo de tv y metodos de cambiar canal y volumen

--- televisores/tv.py
+++ televisores/tv.py
@@ -3,4 +3,67 @@ class TV:
     def __init__(self, marca, estado):
         self.marca = marca
         self.estado = estado
+        self.canal = 1
+        self.precio = 500
+        self.control = None
+        self.volumen = 1
+        TV.numTV += 1
+    
+    #get y set
+    def setMarca(self, marca):
+        self.marca = marca
+    def getMarca(self):
+        return self.marca
+    
+    def setCanal(self, canal):
+        self.canal = canal
+    def getCanal(self):
+        return self.canal
+    
+    def setPrecio(self, precio):
+        self.precio = precio
+    def getPrecio(self):
+        return self.precio
+    
+    def setControl(self, control):
+        self.control = control
+    def getControl(self):
+        return self.control
+    
+    def setVolumen(self, volumen):
         self.volumen = volumen
+    def getVolumen(self):
+        return self.volumen
+    
+    #conteo 
+    @classmethod
+    def setNumTV(cls, numTV):
+        cls.numTV = numTV
+    @classmethod
+    def getNumTV(cls):
+        return cls.numTV
+
+    #metodos: turnUp, turnOff
+    def turnUp(self):
+        self.estado = True
+    def turnOff(self):
+        self.estado = False
+
+    #metodo getEstado
+    def getEstado(self):
+        return self.estado
+    
+    #metodos canalUp, canalDown, volumenUp, volumenDown
+    def canalUp(self):
+        if(self.estado and (self.canal>=1 and self.canal<121)):
+            self.canal += 1
+    def canalDown(self):
+        if(self.estado and (self.canal>1 and self.canal<=120)):
+            self.canal -= 1
+    
+    def volumenUp(self):
+        if(self.estado and (self.volumen>=0 and self.volumen<8)):
+            self.volumen += 1
+    def volumenDown(self):
+        if(self.estado and (self.volumen>0 and self.volumen<=7)):
+            self.volumen -= 1
